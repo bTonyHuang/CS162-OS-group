9a83b06340bb57b6ba22490d94ba50779e45eb65
Daniel Duan <danielduan@berkeley.edu>
2023-05-06 02:49:28 +0000

    modifications to sys_remove for removing directories

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -155,11 +155,33 @@ bool filesys_remove(const char* name) {
   if (container_dir == NULL) {
     return false;
   }
-  bool remove_success = dir_remove(container_dir, filename);
-  /* Resolve incremented open_cnt for container_dir, but now we're done with it. */
-  dir_close(container_dir);
+  struct inode* inode;
+  bool found_entry = dir_lookup(container_dir, filename, &inode);
+  if (!found_entry) {
+    dir_close(container_dir);
+    return false;
+  }
 
-  return remove_success;
+  bool remove_success;
+  if (!inode_is_dir(inode)) {
+    /* If entry is a file, we are good to REMOVE. */
+    remove_success = dir_remove(container_dir, filename);
+    /* Resolve incremented open_cnt for container_dir, but now we're done with it. */
+    dir_close(container_dir);
+
+    return remove_success;
+  } else {
+    /* If entry is a directory, we gotta check its open_cnt first! */
+    if (inode->open_cnt <= 1 && inode->sector != thread_current()->pcb->cwd->inode->sector) {
+      remove_success = dir_remove(container_dir, filename);
+      dir_close(container_dir);
+      return remove_success;
+    } else {
+      /* Open count disallows us from removing the directory. */
+      dir_close(container_dir);
+      return false;
+    }
+  }
 }
 
 /* Changes the current working directory of the current thread to the directory located at PATH.
